1fc2eff7feb7c96176e19c02d1a0a42feece0f3e
Abiyyu Daffa' Alam <daffaalam>
2017-10-15 21:13:09 +0700

    Initial commit

---
+++ app/src/main/java/com/daffaalam/example/autocomplete/main.java
@@ -0,0 +1,41 @@
+package com.daffaalam.example.autocomplete;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+
+public class main extends AppCompatActivity {
+
+    String[] nama = {
+            "Jakarta Selatan",
+            "Jakarta Barat",
+            "Jakarta Timur",
+            "Jakarta Utara",
+            "Jakarta Pusat",
+            "Tangerang Kota",
+            "Tangerang Kabupaten",
+            "Tangerang Selatan",
+            "Bekasi Barat",
+            "Bekasi Selatan",
+            "Bekasi Timur",
+            "Bekasi Utara",
+            "Bogor",
+            "Depok",
+            "Bandung"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AutoCompleteTextView nmakt = (AutoCompleteTextView) findViewById(R.id.autotext);
+
+        ArrayAdapter<String> nmkta = new ArrayAdapter<String>(main.this, android.R.layout.simple_list_item_1, nama);
+
+        nmakt.setThreshold(1);
+        nmakt.setAdapter(nmkta);
+    }
+}
